22626d146ba04368673dea0d9b1faa5aed25e849
johnsonlucas <johnluca@oregonstate.edu>
2020-08-27 17:08:57 -0600

    Comment updates to files.

--- AKT_Input.py
+++ AKT_Input.py
@@ -1,15 +1,18 @@
 # Lucas Johnson
-# Provides the input data for the AKT section of the research report for each fund.
+# The program leverages two csv file imports and user defined variables to define responses used as input data for the
+# AKT process.
 
 import pandas as pd
 import openpyxl
 import csv
 
-# Create a dataframe of the parameter data from the excel spreadsheet.
+# Create a dataframe of the parameter data from the excel spreadsheet. The parameter csv file contains data that is
+# used to define Fees and Performance inputs below.
 param_df = pd.read_csv('y - Parameters.csv')                                           # file import
 param_df.set_index('Identifier', inplace=True)
 
-# Establish variables for the parameters.
+# Establish variables for the parameters used to create the AKT inputs. These must be updated by the user each quarter
+# when the process is performed.
 down_beta = 0.45
 equity_vol_low = 0.1634
 equity_vol_high = 0.1734
@@ -20,20 +23,26 @@ none_corr = 0.1
 low_corr = 0.3
 mod_corr = 0.5
 
-# Create a dataframe of the fund data for iteration.
+# Create a dataframe of the fund data for iteration. The fund data csv file contains data exported from our proprietary
+# database that will be used in addition to the 'parameters' file to establish responses for the AKT process.
 df_2 = pd.read_csv('y - Fund Data.csv')                                                     # file import
 
+# Creates and writes a csv file with the respective AKT input responses for each fund (line item) in df_2.
 with open('z - AKT Data', 'w', newline='') as csvfile:
 
+    # Writes the column headers for the csv file.
     akt_writer = csv.writer(csvfile, delimiter=',', quoting=csv.QUOTE_MINIMAL)
     akt_writer.writerow(['Fund Name', 'Ticker', 'Risk-Ret Profile', 'Tenure', 'Vol Profile', 'Fees', 'Performance',
                          'Eq Corr', 'FI Corr'])
 
+    # Works through each fund to define the input responses saved as variables for the AKT process.
     for row in df_2.itertuples():
 
+        # Creates variables to hold the fund indentification data for the AKT responses.
         fund_name = row[1]
         ticker = row[2]
 
+        # Creates variables to hold the AKT input data for each fund.
         risk_ret = None
         tenure = None
         vol = None
@@ -114,7 +123,10 @@ with open('z - AKT Data', 'w', newline='') as csvfile:
         if row[9] >= mod_corr:
             fi_corr = 'High'
 
+        # Takes the fund identification variables and AKT input variables and writes them into the csv file for the
+        # fund in question.
         akt_writer.writerow([fund_name, ticker, risk_ret, tenure, vol, fees, perform, eq_corr, fi_corr])
 
+# Reads the newly written AKT data csv file and writes it into an excel file.
 read_file = pd.read_csv(r'C:\Users\ljohnson\PycharmProjects\AKT_Updates\z - AKT Data')
 read_file.to_excel(r'C:\Users\ljohnson\PycharmProjects\AKT_Updates\z - AKT Input Data.xlsx', index=None, header=True)
